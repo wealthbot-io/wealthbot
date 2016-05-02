d0462543c3dbf4f395bff84c44d95a90fa3ca05b
Tigran Azatyan <tigran@azatyan.info>
2016-05-02 22:44:59 +0400

    upgrade core files

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -50,6 +50,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new IC\Bundle\Base\TestBundle\ICBaseTestBundle();
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
         }
 
         return $bundles;

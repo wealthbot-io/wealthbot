f5903257ce98848ce5b5d2fc2fe9dfb1f97de9d5
Tigran Azatyan <tigran@azatyan.info>
2016-05-04 11:41:45 +0400

    turn on debug

--- web/app.php
+++ web/app.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
  */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../app/bootstrap.php.cache';
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter

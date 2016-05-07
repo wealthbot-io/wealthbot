0042b5a9e2b3b84fa67c84d80b10cee5544939af
Tigran Azatyan <tigran@azatyan.info>
2016-05-07 19:17:01 +0400

    upgrade

--- src/Wealthbot/AdminBundle/Form/Type/AdminFeesType.php
+++ src/Wealthbot/AdminBundle/Form/Type/AdminFeesType.php
@@ -9,13 +9,12 @@
 
 namespace Wealthbot\AdminBundle\Form\Type;
 
-use Wealthbot\UserBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormError;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Wealthbot\UserBundle\Entity\User;
 
 class AdminFeesType extends AbstractType
 {
@@ -33,20 +32,20 @@ class AdminFeesType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('fees', 'collection', array(
+        $builder->add('fees', 'collection', [
             'type' => new FeeFormType($this->owner),
             'allow_add' => true,
             'allow_delete' => true,
             'prototype' => true,
             'prototype_name' => 'fee__name__',
             'by_reference' => false,
-            'property_path' => false,
-        ));
+           // // 'property_path' => '',
+        ]);
 
-        $builder->addEventListener(FormEvents::BIND, array($this, 'onBind'));
+        $builder->addEventListener(FormEvents::SUBMIT, [$this, 'onSubmit']);
     }
 
-    public function onBind(FormEvent $event)
+    public function onSubmit(FormEvent $event)
     {
         $form = $event->getForm();
         $fees = $form->get('fees')->getData();
@@ -58,42 +57,42 @@ class AdminFeesType extends AbstractType
         foreach ($fees as $key => $fee) {
             $formBottom = round($form->get('fees')->get($key)->get('tier_bottom')->getData(), 2);
 
-            if ($formBottom != $bottom) {
-                if (round($bottom) == $maxTopTierValue) {
+            if ($formBottom !== $bottom) {
+                if (round($bottom) === $maxTopTierValue) {
                     $form->get('fees')->get($key)->get('tier_top')->addError(
-                        new FormError("You already have specified final tier.")
+                        new FormError('You already have specified final tier.')
                     );
                     break;
                 } else {
                     $form->get('fees')->get($key)->get('tier_bottom')->addError(
-                        new FormError("Tier bottom should be %bottom%, %form_bottom% given.", array(
+                        new FormError('Tier bottom should be %bottom%, %form_bottom% given.', [
                             '%bottom%' => number_format($bottom, 2),
-                            '%form_bottom%' => number_format($formBottom, 2)
-                        ))
+                            '%form_bottom%' => number_format($formBottom, 2),
+                        ])
                     );
                 }
             }
 
             if ($formBottom >= $fee->getTierTop()) {
                 $form->get('fees')->get($key)->get('tier_top')->addError(
-                    new FormError("This value must be greater than tier bottom.")
+                    new FormError('This value must be greater than tier bottom.')
                 );
             }
 
             if ($fee->getFeeWithRetirement() >= 1) {
                 $form->get('fees')->get($key)->get('fee_with_retirement')->addError(
-                    new FormError("Fee must be greater than 0 and less then 1 (example : 0.0125).")
+                    new FormError('Fee must be greater than 0 and less then 1 (example : 0.0125).')
                 );
             }
 
             if ($fee->getFeeWithoutRetirement() >= 1) {
                 $form->get('fees')->get($key)->get('fee_without_retirement')->addError(
-                    new FormError("Fee must be greater than 0 and less then 1 (example : 0.0125).")
+                    new FormError('Fee must be greater than 0 and less then 1 (example : 0.0125).')
                 );
             }
 
-            $bottom = round( ($fee->getTierTop() + 0.01), 2);
-            if (round($fee->getTierTop()) == $maxTopTierValue) {
+            $bottom = round(($fee->getTierTop() + 0.01), 2);
+            if (round($fee->getTierTop()) === $maxTopTierValue) {
                 $hasTopTier = true;
             }
 
@@ -103,13 +102,13 @@ class AdminFeesType extends AbstractType
         }
 
         if (!$hasTopTier) {
-            $form->addError(new FormError('You should specify final tier', array(
-                '%bottom%' => $bottom
-            )));
+            $form->addError(new FormError('You should specify final tier', [
+                '%bottom%' => $bottom,
+            ]));
         }
     }
 
-    public function getName()
+    public function getBlockPrefix()
     {
         return 'rx_admin_fee_type';
     }

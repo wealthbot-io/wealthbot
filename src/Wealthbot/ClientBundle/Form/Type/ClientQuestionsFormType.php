773d09edba1d78967bc88ed5487093b8fca1af23
Tigran Azatyan <tigran@azatyan.info>
2016-05-09 10:01:26 +0400

    form types upgrade

--- src/Wealthbot/ClientBundle/Form/Type/ClientQuestionsFormType.php
+++ src/Wealthbot/ClientBundle/Form/Type/ClientQuestionsFormType.php
@@ -2,7 +2,7 @@
 /**
  * User: Maxim O. Belyakov
  * Date: 10.09.12
- * Time: 18:08
+ * Time: 18:08.
  */
 
 namespace Wealthbot\ClientBundle\Form\Type;
@@ -12,7 +12,6 @@ use Wealthbot\RiaBundle\Form\Type\RiskQuestionsFormType as BaseType;
 
 class ClientQuestionsFormType extends BaseType
 {
-
     public function __construct(\Doctrine\ORM\EntityManager $em, \Wealthbot\UserBundle\Entity\User $user, $isPreSave = false)
     {
         parent::__construct($em, $user, false);
@@ -23,45 +22,45 @@ class ClientQuestionsFormType extends BaseType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        foreach($this->questions as $question){
+        foreach ($this->questions as $question) {
             if ($question->getIsWithdrawAgeInput()) {
-                $builder->add('answer_'.$question->getId(), 'text', array(
+                $builder->add('answer_'.$question->getId(), 'text', [
                     'label' => $question->getTitle(),
-                    'property_path' => false,
+                    // 'property_path' => '',
                     'required' => false,
-                    'data' => $this->user->getProfile()->getWithdrawAge()
-                ));
+                    'data' => $this->user->getProfile()->getWithdrawAge(),
+                ]);
             } else {
                 $userAnswer = $this->em->getRepository('WealthbotClientBundle:ClientQuestionnaireAnswer')->createQueryBuilder('ua')
                     ->where('ua.client_id = :client_id AND ua.question_id = :question_id')
-                    ->setParameters(array('client_id' => $this->user->getId(), 'question_id' => $question->getId()))
+                    ->setParameters(['client_id' => $this->user->getId(), 'question_id' => $question->getId()])
                     ->getQuery()
                     ->getOneOrNullResult();
 
                 $userAnswer = $userAnswer ? $userAnswer->getAnswer() : $userAnswer;
 
                 $builder
-                    ->add('answer_'.$question->getId(), 'entity', array(
-                            'class' => 'WealthbotRiaBundle:RiskAnswer',
-                            'query_builder' => function(\Doctrine\ORM\EntityRepository $er) use ($question) {
+                    ->add('answer_'.$question->getId(), 'entity', [
+                            'class' => 'Wealthbot\\RiaBundle\\Entity\\RiskAnswer',
+                            'query_builder' => function (\Doctrine\ORM\EntityRepository $er) use ($question) {
                                 return $er->createQueryBuilder('a')
                                     ->where('a.risk_question_id = :question_id')
                                     ->setParameter('question_id', $question->getId());
                             },
-                            'empty_value' => $userAnswer ? false : 'Choose an Option',
+                            'placeholder' => $userAnswer ? false : 'Choose an Option',
                             'property' => 'title',
-                            'property_path' => false,
+                            // 'property_path' => '',
                             'required' => false,
                             'label' => $question->getTitle(),
-                            'preferred_choices' => $userAnswer ? array($userAnswer) : array()
-                        ));
+                            'preferred_choices' => $userAnswer ? [$userAnswer] : [],
+                        ]);
             }
         }
 
-        $this->addOnBindValidator($builder);
+        $this->addOnSubmitValidator($builder);
     }
 
-    public function getName()
+    public function getBlockPrefix()
     {
         return 'wealthbot_userbundle_client_questions_type';
     }

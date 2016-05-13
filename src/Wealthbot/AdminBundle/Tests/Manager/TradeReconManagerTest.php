517deaae9cd4bc3c5e31ee3ca597cbad88656a25
Tigran Azatyan <tigran@azatyan.info>
2016-05-13 20:00:50 +0400

    update unit test

--- src/Wealthbot/AdminBundle/Tests/Manager/TradeReconManagerTest.php
+++ src/Wealthbot/AdminBundle/Tests/Manager/TradeReconManagerTest.php
@@ -21,31 +21,19 @@ class TradeReconManagerTest extends ExtendedWebTestCase
         $emptyArray = [],
         $wasRebalancerDiffIsSetData = [
             0 => [
-                    'ria' => 'Wealthbot.io - RIA',
-                    'custodian' => 'TD Ameritrade',
-                    'last_name' => 'Jhonny',
-                    'first_name' => 'Cage',
-                    'acct_number' => '480888811',
-                    'symbol' => 'Vanguard Interm-Tm Corp Bd Idx ETF',
-                    'submitted_action' => 'buy',
-                    'executed_action' => '',
-                    'submitted_amount' => 0.0,
-                    'executed_amount' => '',
-                    'error' => true,
-                ],
-            1 => [
-                'ria' => 'Wealthbot.io - RIA',
+                'ria' => 'Mortal Kombat Advisors',
                 'custodian' => 'TD Ameritrade',
-                'last_name' => 'Jhonny',
-                'first_name' => 'Cage',
-                'acct_number' => '744888385',
-                'symbol' => 'SPDR Dow Jones Intl Real Estate',
-                'submitted_action' => '',
-                'executed_action' => 'Buy',
-                'submitted_amount' => '',
-                'executed_amount' => 1996.39,
-                'error' => true,
-            ],
+                'last_name' => 'Kang',
+                'first_name' => 'Liu',
+                'acct_number' => '480888811',
+                'symbol' => 'Vanguard Interm-Tm Corp Bd Idx ETF',
+                'submitted_action' => 'buy',
+                'executed_action' => '',
+                'submitted_amount' => 0.0,
+                'executed_amount' => '',
+                'error' => true
+
+                ]
         ],
         $transactionNotExecutedData = [
 
@@ -54,7 +42,7 @@ class TradeReconManagerTest extends ExtendedWebTestCase
             0 => [
                 'ria' => 'Wealthbot.io - RIA',
                 'custodian' => 'TD Ameritrade',
-                'last_name' => 'Jhonny',
+                'last_name' => 'Johnny',
                 'first_name' => 'Cage',
                 'acct_number' => '744888385',
                 'symbol' => 'SPDR Dow Jones Intl Real Estate',
@@ -69,7 +57,7 @@ class TradeReconManagerTest extends ExtendedWebTestCase
             0 => [
                     'ria' => 'Wealthbot.io - RIA',
                     'custodian' => 'TD Ameritrade',
-                    'last_name' => 'Jhonny',
+                    'last_name' => 'Johnny',
                     'first_name' => 'Cage',
                     'acct_number' => '744888385',
                     'symbol' => 'SPDR Dow Jones Intl Real Estate',
@@ -85,7 +73,7 @@ class TradeReconManagerTest extends ExtendedWebTestCase
     public function setUp()
     {
         parent::setUp();
-        $this->ria = $this->authenticateUser('johnny@wealthbot.io', ['ROLE_RIA']);
+        $this->ria = $this->authenticateUser('johnny@wealthbot.io', ['ROLE_RIA'])->getRia();
         $this->tradeReconManager = $this->container->get('wealthbot_admin.trade_recon.manager');
     }
 
@@ -112,7 +100,7 @@ class TradeReconManagerTest extends ExtendedWebTestCase
     public function testWasRebalancerDiffIsSet()
     {
         $date = '2013-02-13';
-        $this->testResult = $this->tradeReconManager->getValues(new \DateTime($date), new \DateTime($date), $this->ria, 'Jhonny, Cage');
+        $this->testResult = $this->tradeReconManager->getValues(new \DateTime($date), new \DateTime($date), $this->ria, 'Kang, Liu');
         $this->assertSame($this->wasRebalancerDiffIsSetData, $this->testResult, 'Test for Rebalancer Diff failed', 0.001);
     }
 
@@ -129,8 +117,8 @@ class TradeReconManagerTest extends ExtendedWebTestCase
     public function testNotExecutedTransaction()
     {
         $date = '2013-02-14';
-        $this->testResult = $this->tradeReconManager->getValues(new \DateTime($date), new \DateTime($date), $this->ria, 'Jhonny, Cage');
-        $this->assertSame($this->transactionNotExecutedData, $this->testResult, 'Test for not executed transaction failed');
+        $this->testResult = $this->tradeReconManager->getValues(new \DateTime($date), new \DateTime($date), $this->ria, 'Kang, Liu');
+        $this->assertEquals($this->transactionNotExecutedData, $this->testResult, 'Test for not executed transaction failed');
     }
 
     /**
@@ -146,7 +134,7 @@ class TradeReconManagerTest extends ExtendedWebTestCase
     public function testCorrectWork()
     {
         $date = '2013-02-16';
-        $this->testResult = $this->tradeReconManager->getValues(new \DateTime($date), new \DateTime($date), $this->ria, 'Jhonny, Cage');
+        $this->testResult = $this->tradeReconManager->getValues(new \DateTime($date), new \DateTime($date), $this->ria, '');
         $this->assertSame($this->correctWorkData, $this->testResult, 'Test for correct work failed', 0.001);
     }
 
@@ -163,7 +151,7 @@ class TradeReconManagerTest extends ExtendedWebTestCase
     public function testNotPlannedTransaction()
     {
         $date = '2013-03-22';
-        $this->testResult = $this->tradeReconManager->getValues(new \DateTime($date), new \DateTime($date), $this->ria, 'Jhonny, Cage');
+        $this->testResult = $this->tradeReconManager->getValues(new \DateTime($date), new \DateTime($date), $this->ria, 'Kang, Liu');
         $this->assertSame($this->notPlannedTransactionData, $this->testResult, 'Test for empty data failed', 0.001);
     }
 }

63958b1755f57ef6a863f8b7509ae8b2b056fccb
Tigran Azatyan <tigran@azatyan.info>
2016-06-07 14:47:40 +0400

    Insight, fix error

--- src/Wealthbot/SignatureBundle/Controller/DocusignController.php
+++ src/Wealthbot/SignatureBundle/Controller/DocusignController.php
@@ -9,8 +9,6 @@
 namespace Wealthbot\SignatureBundle\Controller;
 
 use Doctrine\ORM\EntityManager;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,7 +25,7 @@ use Wealthbot\SignatureBundle\Model\Envelope;
 use Wealthbot\SignatureBundle\Service\ElectronicSignatureService;
 use Wealthbot\UserBundle\Entity\Document;
 
-class DocusignController extends Controller
+class DocusignController extends BaseSign
 {
     /** @var \Wealthbot\SignatureBundle\Docusign\DocusignSessionPersistence */
     private $api;
@@ -40,16 +38,6 @@ class DocusignController extends Controller
 
     /** @var EntityManager */
     private $em;
-
-    public function setContainer(ContainerInterface $container = null)
-    {
-        parent::setContainer($container);
-
-        $this->api = $this->get('wealthbot_docusign.api_client');
-        $this->signatureManager = $this->get('wealthbot_docusign.document_signature.manager');
-        $this->electronicSignature = $this->get('wealthbot_docusign.electronic_signature_service');
-        $this->em = $this->get('doctrine.orm.entity_manager');
-    }
 
     public function signAction($signature_id)
     {

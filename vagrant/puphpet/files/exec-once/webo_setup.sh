69f50629134671e25da2bc0f19655e25331232d8
Tigran Azatyan <tigran.azatyan@gmail.com>
2016-06-27 15:58:18 +0400

    fix vagrant script

--- vagrant/puphpet/files/exec-once/webo_setup.sh
+++ vagrant/puphpet/files/exec-once/webo_setup.sh
@@ -1,3 +1,13 @@
+#forec php 5 version
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get purge -y php7.0-common
+sudo apt-get autoremove
+sudo apt-get install -y php5.6 php5-mongo php5-apcu php5-cli  php5-intl php5-imagick php5-mcrypt php5-curl
+
+sudo service apache2 restart
+
+
 #disable default vhosts
 echo .... disable default vhost 80 ....
 sudo a2dissite 10-default_vhost_80.conf

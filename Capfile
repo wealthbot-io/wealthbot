c103973a15f7e7b9fd6188fa341b7a175a80bdaf
vladko <vlad.kobilansky@gmail.com>
2017-04-21 13:32:28 -0400

    adjusting index

--- Capfile
+++
@@ -1,4 +0,0 @@
-load 'deploy' if respond_to?(:namespace)
-
-require 'capifony_symfony2'
-load 'app/config/deploy'
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m "Fixes after Twitter Bootstrap Upgrade, $(date)"
-git push
-cap demo deploy

a00dd9ef09408127977bbbae57f410e2459a2c17
tigran@azatyan.info <tigran@azatyan.info>
2019-01-11 17:18:30 +0400

    docker file

--- startup.sh
+++ startup.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-
-cd /var/www/symfony
-chmod -R 0777 app/cache app/logs
-composer clear-cache
-composer install
-mongo < mongo_user.js
-mongo < mongo_user_test.js
-app/console doctrine:database:drop --force
-app/console doctrine:database:create
-app/console doctrine:schema:create
-app/console doctrine:fixtures:load --append
-app/console assetic:dump
+# Temp. comment (TODO: remove comments after fixation of composer dependencies)
+#chmod -R 0777 app/cache app/logs
+#composer clear-cache
+#composer install
+#
+#app/console doctrine:database:drop --force
+#app/console doctrine:database:create
+#app/console doctrine:schema:create
+#app/console doctrine:fixtures:load --append
+#app/console assetic:dump
 

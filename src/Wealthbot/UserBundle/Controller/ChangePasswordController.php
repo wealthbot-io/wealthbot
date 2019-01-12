6db370093be1ed1754d9a4780dbe6b46b8da441c
tigran@azatyan.info <tigran@azatyan.info>
2019-01-12 18:46:30 +0400

    fix controller

--- src/Wealthbot/UserBundle/Controller/ChangePasswordController.php
+++ src/Wealthbot/UserBundle/Controller/ChangePasswordController.php
@@ -12,7 +12,7 @@
 namespace Wealthbot\UserBundle\Controller;
 
 use Doctrine\ORM\EntityManager;
-use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use FOS\UserBundle\Controller\ChangePasswordController as BaseController;
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,31 +28,6 @@ use Wealthbot\RiaBundle\Mailer\MailerInterface;
  */
 class ChangePasswordController extends BaseController
 {
-    /**
-     * Change user password.
-     */
-    public function changePasswordAction()
-    {
-        $user = $this->container->get('security.token_storage')->getToken()->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-        $form = $this->container->get('fos_user.change_password.form');
-        $formHandler = $this->container->get('fos_user.change_password.form.handler');
-
-        $process = $formHandler->process($user);
-        if ($process) {
-            $this->setFlash('fos_user_success', 'change_password.flash.success');
-
-            return new RedirectResponse($this->getRedirectionUrl($user));
-        }
-
-        return $this->container->get('templating')->renderResponse(
-            'FOSUserBundle:ChangePassword:changePassword.html.'.$this->container->getParameter('fos_user.template.engine'),
-            ['form' => $form->createView()]
-        );
-    }
-
     public function resetPasswordAction(Request $request)
     {
         /** @var $em EntityManager */
@@ -89,23 +64,6 @@ class ChangePasswordController extends BaseController
 
     protected function setFlash($action, $value)
     {
-        $this->container->get('session')->getFlashBag()->add($action, $value);
-    }
-
-    /**
-     * Returns a NotFoundHttpException.
-     *
-     * This will result in a 404 response code. Usage example:
-     *
-     *     throw $this->createNotFoundException('Page not found!');
-     *
-     * @param string    $message  A message
-     * @param Exception $previous The previous exception
-     *
-     * @return NotFoundHttpException
-     */
-    protected function createNotFoundException($message = 'Not Found', \Exception $previous = null)
-    {
-        return new NotFoundHttpException($message, $previous);
+        $this->container->get('session')-> getFlashBag()->add($action, $value);
     }
 }

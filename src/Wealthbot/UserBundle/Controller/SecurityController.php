8f255b43aa5a25a931ad6861f646bb36c3a5decd
tigran@azatyan.info <tigran@azatyan.info>
2019-01-13 13:17:07 +0400

    fix

--- src/Wealthbot/UserBundle/Controller/SecurityController.php
+++ src/Wealthbot/UserBundle/Controller/SecurityController.php
@@ -115,4 +115,27 @@ class SecurityController extends BaseSecurity
             ]
         );
     }
+
+    public function checkAction()
+    {
+        throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
+    }
+
+    public function logoutAction()
+    {
+        throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
+    }
+
+    /**
+     * Renders the login template with the given parameters. Overwrite this function in
+     * an extended controller to provide additional data for the login template.
+     *
+     * @param array $data
+     *
+     * @return Response
+     */
+    protected function renderLogin(array $data)
+    {
+        return $this->render('WealthbotUserBundle:Security:login.html.twig', $data);
+    }
 }

3f7bf3361cf6b072887ad3a8a91190657f0ee5bd
Tigran Azatyan <tigran.azatyan@gmail.com>
2019-07-05 18:47:12 +0400

    first test

---
+++ tests/ApplicationAvailabilityTest.php
@@ -0,0 +1,26 @@
+<?php
+// tests/ApplicationAvailabilityFunctionalTest.php
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApplicationAvailabilityFunctionalTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function urlProvider()
+    {
+        yield '/';
+        // ...
+    }
+}

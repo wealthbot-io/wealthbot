1615408ac2d787dd3f636096f352cf5981980e22
Tigran Azatyan <tigran.azatyan@gmail.com>
2019-07-09 15:43:20 +0400

    Rebalancer

--- system/bin/fakeRebalancer.php
+++ system/bin/fakeRebalancer.php
@@ -2,11 +2,7 @@
 <?php
 $params = getopt('j:');
 $jobId = $params['j'];
-
 set_time_limit(0);
-
-require_once __DIR__.'/WealthbotRebalancer.php';
-
-$rebalancer = new \Console\WealthbotRebalancer();
+$rebalancer = new System\Console\WealthbotRebalancer();
 $rebalancer->fakeStart($jobId);
 

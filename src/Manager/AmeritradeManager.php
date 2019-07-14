84f75857731fc09ee6a61e83eb93e31720b9b4c3
Tigran Azatyan <tigran.azatyan@gmail.com>
2019-07-14 21:57:01 +0400

    Ameritrade integration

---
+++ src/Manager/AmeritradeManager.php
@@ -0,0 +1,86 @@
+<?php
+namespace App\Manager;
+
+use Doctrine\ORM\EntityManager;
+use GuzzleHttp\Client;
+use Monolog\Logger;
+
+class AmeritradeManager {
+
+    private $httpClient;
+
+    private $em;
+
+    private $logger;
+
+    private $apiGateway;
+
+    private $apiKey;
+
+
+    /**
+     * AmeritradeManager constructor.
+     * @param Client $guzzleHttp
+     * @param EntityManager $entityManager
+     * @param Logger $logger
+     */
+    public function __construct(Client $guzzleHttp, EntityManager $entityManager, Logger $logger)
+    {
+
+        $this->httpClient = $guzzleHttp;
+        $this->em = $entityManager;
+        $this->logger;
+        $this->apiGateway = "https://api.tdameritrade.com/v1/";
+    }
+
+    /**
+     * @param $key
+     */
+    public function setApiKey($key){
+
+        $this->apiKey = $key;
+    }
+
+    private function createRequest($data){
+
+    }
+
+
+    public function createAccount(){
+
+    }
+
+
+    public function getAccount(){
+
+    }
+
+
+    public function editAccount(){
+
+    }
+
+
+    public function deleteAccount(){
+
+    }
+
+
+
+    public function placeOrder(){
+
+    }
+
+
+
+    public function getOrder(){
+
+    }
+
+
+
+    public function cancelOrder(){
+
+    }
+
+}

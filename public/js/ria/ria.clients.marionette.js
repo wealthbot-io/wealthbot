f2e4195024ba679852e1a8b9325d033f28c14e28
Tigran Azatyan <tigran.azatyan@gmail.com>
2019-07-17 20:37:11 +0400

    Fix ajax loader

--- public/js/ria/ria.clients.marionette.js
+++ public/js/ria/ria.clients.marionette.js
@@ -352,7 +352,7 @@ App.module('Wealthbot.Clients', function(Mod, App, Backbone, Marionette, $) {
                     },
                     complete: function() {
                         submitButton
-                            .empty();
+                            .empty().text('Save');
                     }
                 });
             }
@@ -390,7 +390,7 @@ App.module('Wealthbot.Clients', function(Mod, App, Backbone, Marionette, $) {
                     },
                     complete: function() {
                         submitButton
-                            .empty();
+                            .empty().text('Save');
                     }
                 });
             }

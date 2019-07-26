fa808a0371090ffb5a19fae81e00ea1183803d57
Tigran Azatyan <tigran.azatyan@gmail.com>
2019-07-26 10:47:45 +0400

    Rebalancer Interface

--- src/Api/RebalancerInterface.php
+++ src/Api/RebalancerInterface.php
@@ -4,9 +4,15 @@
 namespace App\Api;
 
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 interface RebalancerInterface
 {
 
+    public function __construct(EntityManagerInterface $entityManager, ContainerInterface $container, \Symfony\Component\Security\Core\Security $security);
+
     public function rebalance();
 
+    public function updateSecurities();
 }
